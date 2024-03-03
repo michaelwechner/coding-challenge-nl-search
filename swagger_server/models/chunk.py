5e8703dcbb5485305c8a8b24d016fbc2852bbfce
Michael Wechner <michael.wechner@wyona.com>
2024-03-03 17:16:30 +0100

    text chunk

---
+++ swagger_server/models/chunk.py
@@ -0,0 +1,9 @@
+import json
+
+class Chunk(dict):
+    def __init__(self, text: str=None):
+        dict.__init__(self, text=text)
+        self.text = text
+
+    def toJson(self):
+        return json.dumps(self, default=lambda o: o.__dict__)
