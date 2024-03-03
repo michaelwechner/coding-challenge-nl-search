f7da86527b0e7bcb1dbb8585b6601395b737df7c
Michael Wechner <michael.wechner@wyona.com>
2024-03-03 16:50:18 +0100

    dummy response added

--- swagger_server/controllers/search_documents_using_natural_language_controller.py
+++ swagger_server/controllers/search_documents_using_natural_language_controller.py
@@ -5,6 +5,7 @@ from swagger_server.models.search_output import SearchOutput  # noqa: E501
 from swagger_server import util
 
 from flask import jsonify
+from swagger_server.models.hit import Hit
 
 
 def api_v1_search_get(query):  # noqa: E501
@@ -18,5 +19,11 @@ def api_v1_search_get(query):  # noqa: E501
     :rtype: SearchOutput
     """
 
-    response = {'query': query, 'llm':'TODO: For example Mistral', 'results':'[{id=5, score=0.8767, text=text}]'}
+    hit1 = Hit('Michael was born 1969', 0.8689, 56)
+    hit2 = Hit('Michael was born in St. Gallen', 0.5427, 34)
+    hit3 = Hit('Vanya was born 2001', 0.2677, 17)
+    results = [hit1, hit2, hit3]
+    #results = [hit1.text, hit2.text, hit3.text]
+
+    response = {'query': query, 'llm': 'TODO: For example Mistral', 'results': results}
     return jsonify(response)
