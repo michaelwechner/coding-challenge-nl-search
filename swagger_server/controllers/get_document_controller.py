4e04538f28dbcb77275d94c7c1e96a871c44fb99
Michael Wechner <michael.wechner@wyona.com>
2024-03-03 17:16:58 +0100

    return mockup response

--- swagger_server/controllers/get_document_controller.py
+++ swagger_server/controllers/get_document_controller.py
@@ -4,15 +4,24 @@ import six
 from swagger_server.models.get_document_output import GetDocumentOutput  # noqa: E501
 from swagger_server import util
 
+from flask import jsonify
+from swagger_server.models.chunk import Chunk
 
-def api_v1_documents_id_get(id):  # noqa: E501
+
+def api_v1_documents_id_get(id_):  # noqa: E501
     """Returns content of a particular document from document store
 
      # noqa: E501
 
-    :param id: ID of document
-    :type id: int
+    :param id_: ID of document
+    :type id_: int
 
     :rtype: GetDocumentOutput
     """
-    return 'do some magic!'
+
+    chunk1 = Chunk('Michael was born 1969')
+    chunk2 = Chunk('Michael was born in St. Gallen')
+    chunks = [chunk1, chunk2]
+
+    response = {'id': id_, 'chunks': chunks}
+    return jsonify(response)
