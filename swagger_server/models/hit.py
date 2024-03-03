fef6809c86dc813b3d698a54f722a6b7e2180cdf
Michael Wechner <michael.wechner@wyona.com>
2024-03-03 16:49:49 +0100

    hit class started

---
+++ swagger_server/models/hit.py
@@ -0,0 +1,11 @@
+import json
+
+class Hit(dict):
+    def __init__(self, text: str=None, score: float=None, id: int=None):
+        dict.__init__(self, text=text, score=score, id=id)
+        self.text = text
+        self.score = score
+        self.id = id
+
+    def toJson(self):
+        return json.dumps(self, default=lambda o: o.__dict__)
