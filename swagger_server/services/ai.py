08a9cf143bd94ed2edece7c4603027033707da43
Michael Wechner <michael.wechner@wyona.com>
2024-03-04 08:14:57 +0100

    OpenAI embedding added

--- swagger_server/services/ai.py
+++ swagger_server/services/ai.py
@@ -11,13 +11,21 @@ class AI(dict):
         self.cohere = Client(api_key=os.environ.get("COHERE_API_KEY"))
 
     def getEmbedding(self, text: str=None, input_type: str="search_document"):
+        # TODO: Make embedding model 'text-embedding-ada-002' or 'embed-multilingual-v3.0' configurable
+
         # INFO: Cohere implementation
         texts = [text]
         #texts = [text, "Hello World"]
         cohere = Client(api_key=os.environ.get("COHERE_API_KEY"))
+        # https://cohere-sdk.readthedocs.io/en/latest/cohere.html#cohere.client.Client.embed resp. https://docs.cohere.com/reference/embed
         embeddings = cohere.embed(texts=texts, model="embed-multilingual-v2.0") # Length: 768
         #embeddings = self.cohere.embed(texts=texts, model="embed-multilingual-v2.0") # Length: 768
+        #print(f"Cohere Embeddings: {embeddings}")
         embedding = embeddings.embeddings[0]
+
+        # INFO: OpenAI implementation
+        #client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+        #embedding = client.embeddings.create(input=body.text, model="text-embedding-ada-002")
 
         # INFO: Mock implementation
         #embedding = [0.1, 0.15, 0.3, 0.12, 0.05]
