e987977d110b81a00d64e3d4e76116109052d2e2
Michael Wechner <michael.wechner@wyona.com>
2024-03-07 10:26:35 +0100

    use data repo service

--- swagger_server/controllers/get_document_controller.py
+++ swagger_server/controllers/get_document_controller.py
@@ -1,13 +1,8 @@
-import connexion
-import six
-import os
-
 from swagger_server.models.get_document_output import GetDocumentOutput  # noqa: E501
-from swagger_server import util
 
 from flask import jsonify
 from swagger_server.models.chunk import Chunk
-from astrapy.db import AstraDB
+from swagger_server.services.datarepository import DataRepository
 
 
 def api_v1_documents_id_get(id_):  # noqa: E501
@@ -21,23 +16,7 @@ def api_v1_documents_id_get(id_):  # noqa: E501
     :rtype: GetDocumentOutput
     """
 
-    db = AstraDB(
-        token=os.getenv("ASTRA_DB_APPLICATION_TOKEN"),
-        api_endpoint=os.getenv("ASTRA_DB_API_ENDPOINT"),
-        namespace="default_keyspace",
-    )
-    print(db)
-
-    db_collection_name=os.getenv("ASTRA_DB_COLLECTION")
-    print(f"DB Collection Name: {db_collection_name}")
-
-    collection = db.collection(db_collection_name)
-    print(f"Get document {id_} ...")
-    results = collection.find({"_id": id_})
-    chunks = []
-    for document in results["data"]["documents"]:
-        print(document['text'])
-        chunks.append(document['text'])
+    chunks = DataRepository().getText(id_=id_)
 
     #chunk1 = Chunk('Michael was born 1969')
     #chunk2 = Chunk('Michael was born in St. Gallen')
