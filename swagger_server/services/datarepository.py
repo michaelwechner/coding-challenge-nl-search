a67a95eb57926b29e9bb7dfd29632d6fc4390991
Michael Wechner <michael.wechner@wyona.com>
2024-03-07 10:03:01 +0100

    data repo service started

---
+++ swagger_server/services/datarepository.py
@@ -0,0 +1,37 @@
+# AI Service
+
+import os
+
+from astrapy.db import AstraDB
+from swagger_server.services.ai import AI
+from swagger_server.models.hit import Hit
+
+class DataRepository(dict):
+    def __init__(self):
+        # TODO: Singleton
+        self.db = AstraDB(
+            token=os.getenv("ASTRA_DB_APPLICATION_TOKEN"),
+            api_endpoint=os.getenv("ASTRA_DB_API_ENDPOINT"),
+            namespace="default_keyspace",
+        )
+        print(self.db)
+
+        db_collection_name=os.getenv("ASTRA_DB_COLLECTION")
+        print(f"DB Collection Name: {db_collection_name}")
+
+        self.collection = self.db.collection(db_collection_name)
+
+    def search(self, query: str):
+        print(f"Get embedding for query text '{query}' ...")
+        query_embedding = AI().getEmbedding(text=query, input_type="search_query")
+        print(f"Query embedding: {query_embedding}")
+
+        db_results = self.collection.vector_find(vector=query_embedding, limit=10)
+        hits = []
+        for document in db_results:
+            print(f"Found document: {document}")
+            hit = Hit(document['text'], document['$similarity'], document['_id'])
+            hits.append(hit)
+
+        return hits
+
