a1d89da1f32f2137d00b58abaf949334b6dcc239
Michael Wechner <michael.wechner@wyona.com>
2024-03-07 10:19:29 +0100

    use data repo service

--- swagger_server/controllers/upload_plain_text_document_controller.py
+++ swagger_server/controllers/upload_plain_text_document_controller.py
@@ -1,15 +1,10 @@
 import connexion
-import six
-import os
-import uuid
 
 from swagger_server.models.upload_document_input import UploadDocumentInput  # noqa: E501
 from swagger_server.models.upload_document_output import UploadDocumentOutput  # noqa: E501
-from swagger_server import util
 
 from flask import jsonify
-from astrapy.db import AstraDB
-from swagger_server.services.ai import AI
+from swagger_server.services.datarepository import DataRepository
 
 
 def api_v1_documents_post(body=None):  # noqa: E501
@@ -25,36 +20,7 @@ def api_v1_documents_post(body=None):  # noqa: E501
     if connexion.request.is_json:
         body = UploadDocumentInput.from_dict(connexion.request.get_json())  # noqa: E501
 
-    #token=os.environ["ASTRA_DB_APPLICATION_TOKEN"]
-    #token=os.environ.get("ASTRA_DB_APPLICATION_TOKEN")
-    token=os.getenv("ASTRA_DB_APPLICATION_TOKEN")
-    print(f"Token: {token}")
-
-    #api_endpoint=os.environ["ASTRA_DB_API_ENDPOINT"]
-    #api_endpoint=os.environ.get("ASTRA_DB_API_ENDPOINT")
-    api_endpoint=os.getenv("ASTRA_DB_API_ENDPOINT")
-    print(f"API Endpoint: {api_endpoint}")
-
-    db_collection_name=os.getenv("ASTRA_DB_COLLECTION")
-    print(f"DB Collection Name: {db_collection_name}")
-
-    db = AstraDB(
-        token=token,
-        api_endpoint=api_endpoint,
-        namespace="default_keyspace",
-    )
-    print(db)
-
-    print(f"Insert text: {body.text}")
-    id = str(uuid.uuid4())
-
-    embedding = AI().getEmbedding(body.text, input_type="search_document")
-    print(f"Vector embedding dimension: {len(embedding)}")
-
-    documents = [{"_id": id,"text": body.text, "$vector": embedding}]
-    print(f"Insert document(s): {documents}")
-    collection = db.collection(db_collection_name)
-    res = collection.insert_many(documents)
+    id = DataRepository().memorizeText(text=body.text)
 
     response = {'id': id}
     return jsonify(response)
